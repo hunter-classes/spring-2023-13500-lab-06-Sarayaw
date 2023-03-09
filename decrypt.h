8bea107ae4183fc998453e48cddeda7e9558c0fa
Sarayaw <104982620+Sarayaw@users.noreply.github.com>
2023-03-09 18:06:26 -0500

    Create decrypt.h

---
+++ decrypt.h
@@ -0,0 +1,9 @@
+#pragma once
+
+char decryptCaesarChar(char c, int rshift);
+
+char decryptVigenereChar(char c, char key);
+
+std::string decryptCaesar(std::string ciphertext, int rshift);
+
+std::string decryptVigenere(std::string ciphertext, std::string keyword);

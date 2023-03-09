c91628e44cbd44f678de4a0f18dcdcc0774d7251
Sarayaw <104982620+Sarayaw@users.noreply.github.com>
2023-03-09 18:09:26 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
-
-#include "funcs.h"
+#include "caeser.h"
+#include "vigenere.h"
+#include "decrypt.h"
 
 int main()
 {
+  std::cout << "Looking at a computer screen to long can hurt your head.\n";
+  (encryptCaesar("ur lying!", 2) == "ur lying!"); ? std::cout << "-> Success!\n"  : std::cout << "-> Failed\n";
+
   return 0;
 }

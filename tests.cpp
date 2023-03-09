16b042a3b3d4b8d5418bd4f3cf146aea96d5a43f
Sarayaw <104982620+Sarayaw@users.noreply.github.com>
2023-03-09 17:49:13 -0500

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,32 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-#include "funcs.h"
+#include "caeser.h"
+#include "vigenere.h"
+#include "decrypt.h"
 
 // add your tests here
+TEST_CASE("Caesar Cipher Theory Tests") {
+  SUBCASE("Basic") {
+    CHECK(encryptCaesar("ur lying!", 2) == "ur lying!");  
+  }
+}
+
+TEST_CASE("Decrypt Caesar Cipher Theory Tests") {
+  SUBCASE("Basic") {
+    CHECK(decryptCaesar("stop it", 2) == "stop it");
+  }
+}
+
+TEST_CASE("Vigenere Cipher Theory Tests") {
+  SUBCASE("Basic") {
+    CHECK(encryptVigenere("Hello, World!", "CAKE") == "Jevpq, Wyvnd!");
+
+  }
+}
+
+TEST_CASE("Decrypt Vigenere Cipher Theory Tests") {
+  SUBCASE("Basic") {
+    CHECK(decryptVigenere("Jevpq, Wyvnd!","cake") == "Hello, World!");
+  }
+
+}

e5fce54e7d88d86ea2926af64dc91d9347fade10
Sarayaw <104982620+Sarayaw@users.noreply.github.com>
2023-03-09 17:35:07 -0500

    Update test-ascii.cpp

--- test-ascii.cpp
+++ test-ascii.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
+#include <cctype>
 #include <string>
 
-void asciiValue()
-{
-    std::string sentence;                      
-    std::cout << "Input" << std::endl;          
-    getline(std::cin, sentence);                
-
-    for (char c : sentence)                      
-    {
-        std::cout << c << " " << static_cast<int>(c) << std::endl;
-    }
+std::string convert(const std::string& str) {
+  std::string output;
+  for(const auto& c : str) {
+    output += c;
+    output += "\t";
+    output += std::to_string(static_cast<int>(c));
+    output += "\n";
+  }
+  return output;
 }
 
-int main()
-{
-    asciiValue();
-    return 0;
+int main() {
+  std::cout << "Test 1\n";
+  std::cout << convert("Hello World") << '\n';
+  
+  std::cout << "Test 2\n";
+  std::cout << convert("I have to study") << '\n';
+  
+  std::cout << "Test 3\n";
+  std::cout << convert("i want to sleep") << '\n';
+
+  return 0;
 }
